27735e4020f3b5a93b6264c072830a87663696c8
Amber Ochoa <127437955+amochalatte@users.noreply.github.com>
2024-08-16 21:17:16 -0500

    docstrings & type hinting

--- television.py
+++ television.py
@@ -19,7 +19,57 @@ class Television:
 
     def channel_up(self):
         if self.__status:
-            if self.__channel < Television.MAX_CHANNEL:
+            if self.__channel < Televisclass Television:
+    MIN_VOLUME: int = 0
+    MAX_VOLUME: int = 2
+    MIN_CHANNEL: int = 0
+    MAX_CHANNEL: int = 3
+
+    def __init__(self) -> None:
+        """Initialize the Television with default settings."""
+        self.__status: bool = False
+        self.__muted: bool = False
+        self.__volume: int = Television.MIN_VOLUME
+        self.__channel: int = Television.MIN_CHANNEL
+
+    def power(self) -> None:
+        """Toggle the power status of the Television."""
+        self.__status = not self.__status
+
+    def mute(self) -> None:
+        """Toggle the mute status of the Television if it's on."""
+        if self.__status:
+            self.__muted = not self.__muted
+
+    def channel_up(self) -> None:
+        """Increase the channel by 1, or loop to MIN_CHANNEL if at MAX_CHANNEL."""
+        if self.__status:
+            self.__channel = (self.__channel + 1) % (Television.MAX_CHANNEL + 1)
+
+    def channel_down(self) -> None:
+        """Decrease the channel by 1, or loop to MAX_CHANNEL if at MIN_CHANNEL."""
+        if self.__status:
+            self.__channel = (self.__channel - 1) % (Television.MAX_CHANNEL + 1)
+
+    def volume_up(self) -> None:
+        """Increase the volume by 1, unmuting if necessary, up to MAX_VOLUME."""
+        if self.__status:
+            self.__muted = False
+            if self.__volume < Television.MAX_VOLUME:
+                self.__volume += 1
+
+    def volume_down(self) -> None:
+        """Decrease the volume by 1, unmuting if necessary, down to MIN_VOLUME."""
+        if self.__status:
+            self.__muted = False
+            if self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
+
+    def __str__(self) -> str:
+        """Return the current state of the Television as a string."""
+        volume_display = Television.MIN_VOLUME if self.__muted else self.__volume
+        return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {volume_display}'
+ion.MAX_CHANNEL:
                 self.__channel += 1
             else:
                 self.__channel = Television.MIN_CHANNEL
